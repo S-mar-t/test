d4564bb6325dd9cbd6d94af5394dbdde9b8f8601
S-mar-t <zohaib4567ahmed@gmail.com>
2024-05-24 06:50:27 -0700

    some practice

--- zohaib.py
+++ zohaib.py
@@ -1,9 +1,14 @@
-light = input("light :")
-if light == "red":
-    print("stop")
-elif light == "yellow":
-    print("look around")
-elif light == "green":
-        print("go")
-else:
-    print("light is broken or damaged")
+#practice session
+#take two values as input and output their sum
+'''first = int(input("enter first number :"))
+second = int(input("enter second number :"))
+print ("sum =" ,first + second)'''
+
+    #write a program to input side of its square & print its area
+'''side = float (input("enter side of square :"))
+print ("area =", side * side)'''
+  #write a program that takes two floating no. as input & find their average
+a = float(input("enter number a :"))
+b = float(input("enter number b :"))
+avg = (a+b)/2
+print ("average =", avg)
