f98fcb64479a7aa0c8f7df03a662774c738f8188
S-mar-t <zohaib4567ahmed@gmail.com>
2024-05-29 10:37:40 -0700

    chapter 1 ends

--- zohaib.py
+++ zohaib.py
@@ -8,7 +8,11 @@ print ("sum =" ,first + second)'''
 '''side = float (input("enter side of square :"))
 print ("area =", side * side)'''
   #write a program that takes two floating no. as input & find their average
-a = float(input("enter number a :"))
+'''a = float(input("enter number a :"))
 b = float(input("enter number b :"))
 avg = (a+b)/2
-print ("average =", avg)
+print ("average =", avg)'''
+  #WAP that takes 2 (a & b) numbers as input and print true if a>=b if not print false
+a = int(input("input number a :"))
+b = int(input("input number b :"))
+print (a>=b)
