349a40fca2a2821936cf34a2a5392425ffd45bf1
S-mar-t <zohaib4567ahmed@gmail.com>
2024-06-19 01:16:35 -0700

    chapter 2 ends

--- two.py
+++ two.py
@@ -1,21 +1,36 @@
-# name=input("enter your first name:")
+ # name=input("enter your first name:")
 # print ("length of first name is :",len(name))
 
 # str1 = ("Hi$, i am a $dollar symbol$ $ 0.99")
-# print (str1.count ("$"))
-
-# use of if/elif/else statements
- # WAP that takes input marks and assign grades of students
+ # print (str1.count ("$"))
+ # use of if/elif/else statements
+  # WAP that takes input marks and assign grades of students
  
-marks= int(input("enter marks of students:"))
+# marks= int(input("enter marks of students:"))
 
-if (marks>=80 and marks>90):
-    grade="A"
-elif (marks>=70 and marks<80):
-    grade = "B"
-elif (marks >=60 and marks<70):
-    grade= "C"
-else :
-    grade= "F"
+# if (marks>=80 and marks>90):
+#     grade="A"
+# elif (marks>=70 and marks<80):
+#     grade = "B"
+# elif (marks >=60 and marks<70):
+#     grade= "C"
+# else :
+#     grade= "F"
 
-print("grade of a student -->",grade)
+# print("grade of a student -->",grade)
+ # WAP to check that the number is odd or even
+# number = int(input("enter number :"))
+# if (number /2):
+#    print("number is even")
+# else:
+#    print("number is odd") 
+ # WAP to find the greatest of 3given numbers
+num1 = int(input("enter num 1 :"))
+num2 = int(input( "enter num 2 :"))
+num3 = int(input("enter num 3 :"))
+if (num1>num2 and num1>num3):
+    print("num1 is greatest",num1)
+elif (num2>num1 and num2>num3):
+    print("num2 is gratest",num2)
+elif (num3>num1 and num3>num2):
+    print("num3 is greatest", "num3")
